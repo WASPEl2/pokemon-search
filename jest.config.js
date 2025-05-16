5a2b8108fafdd92814b6e7f341edb065603d519d
WASPEi2 <aekapab10@gmail.com>
2025-05-16 13:54:33 +0700

    feat(): jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
-const { createDefaultPreset } = require("ts-jest/presets")
-
-const tsJestTransformCfg = createDefaultPreset().transform
-
 /** @type {import("jest").Config} **/
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
 };
